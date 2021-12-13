393b2af672928ef7386b6d0baab748e55cd4cb7d
nao <b1018080@fun.ac.jp>
2021-12-13 11:33:35 +0900

    canvas lift

--- GazeTrackerView.py
+++ GazeTrackerView.py
@@ -11,13 +11,14 @@ class View():
 
         #self.canvas.create_polygon(10,10,10,60,50,35,tag="id1")
         self.canvas.create_oval(self.model.width/2 - 20, self.model.height/2 - 20, self.model.width/2 + 20, self.model.height/2 + 20, tag="viewpoint")
-
+        self.canvas.lift("viewpoint")
+        #self.canvas.config(bg="")
         # Canvasにマウスイベント（左ボタンクリック）の追加
         self.canvas.bind('<Button-1>', self.canvas_click)
         # Canvasを配置
         self.canvas.pack(expand = True, fill = tk.BOTH)
 
-        self.capture = cv2.VideoCapture(1)
+        self.capture = cv2.VideoCapture(0)
 
         self.disp_id = None
 
